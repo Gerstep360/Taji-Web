91c1e9de2120492fe37bd4a45957cb9f9a8552b1
Gerstep <rojascruzgermanlino@gmail.com>
2026-09-07 00:22:34 -0400

    fix(web): remove emojis, replace with svg icons and add smooth loading bar

--- src/app/shared/ui/icon.component.ts
+++ src/app/shared/ui/icon.component.ts
@@ -177,6 +177,20 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
           <polyline points="7 10 12 15 17 10"></polyline>
           <line x1="12" y1="15" x2="12" y2="3"></line>
         }
+        @case ('loader') {
+          <line x1="12" y1="2" x2="12" y2="6"></line>
+          <line x1="12" y1="18" x2="12" y2="22"></line>
+          <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"></line>
+          <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"></line>
+          <line x1="2" y1="12" x2="6" y2="12"></line>
+          <line x1="18" y1="12" x2="22" y2="12"></line>
+          <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"></line>
+          <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"></line>
+        }
+        @case ('inbox') {
+          <polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
+          <path d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path>
+        }
       }
     </svg>
   `,
